fbfe4c7f1b9979285d82c6d7ef846d2abb31170b
nfunk <nf4313@hotmail.com>
2024-10-29 22:07:58 -0500

    Added 5 customers to the Customers table.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer < ApplicationRecord
 
     validates :fullname, presence: true
+    validates :phone, length: {minimum: 10, maximum: 10}
+    # validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
 
     def self.ransackable_attributes(auth_object = nil)
         ["created_at", "email", "fullname", "id", "notes", "phone", "image", "updated_at"]
